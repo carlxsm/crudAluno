da46f7df1ffc34e627b552d37eeeb22ccfc71d20
cmendes <carlos.mendes@dcx.ufpb.br>
2025-11-04 16:55:53 -0300

    CRUD Aluno e Professor

--- api/src/main/java/br/com/alunoonline/api/model/Professor.java
+++ api/src/main/java/br/com/alunoonline/api/model/Professor.java
@@ -1,4 +1,23 @@
 package br.com.alunoonline.api.model;
 
-public class Professor {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Professor implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String cpf;
 }
--- api/src/main/java/br/com/alunoonline/api/repository/ProfessorRepository.java
+++ api/src/main/java/br/com/alunoonline/api/repository/ProfessorRepository.java
@@ -1,4 +1,9 @@
 package br.com.alunoonline.api.repository;
 
-public class ProfessorRepository {
+import br.com.alunoonline.api.model.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 }
